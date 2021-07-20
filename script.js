e9ccc62f1557de938a480c40a7a18918c336c0bb
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-07-20 11:30:40 -0300

    insert hour, minute and second

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let digitalElement = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
-let sElement = document.querySelector('.p_m');
-let sElement = document.querySelector('.p_h');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+
+function updateClock(){
+  let now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  digitalElement.innerHTML = `${hour}:${minute}:${second}`;
+
+}
+
+
+setInterval(updateClock, 1000);
