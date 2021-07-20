b76af14534f8b67ca5b7546d55ba26e2d0314d63
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-07-20 11:37:19 -0300

    factoring if/else

--- script.js
+++ script.js
@@ -10,16 +10,12 @@ function updateClock(){
   let minute = now.getMinutes();
   let second = now.getSeconds();
 
-  digitalElement.innerHTML = `${hour}:${minute}:${second}`;
+  digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
 }
 
 function fixZero(time){
-  if (time < 10) {
-    return '0'+time;
-  } else {
-    return time;
-  }
+  return time < 10 ? `0${time}` : time;
 }
 
 setInterval(updateClock, 1000);
