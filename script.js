f4e5cb28adf4011c4383900d35b74472972f6288
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-07-20 11:22:52 -0300

    create variables h, m and s

---
+++ script.js
@@ -0,0 +1,4 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let sElement = document.querySelector('.p_m');
+let sElement = document.querySelector('.p_h');
